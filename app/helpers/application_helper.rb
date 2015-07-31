615d01f4f87773adbad10f35c26da85f30c442f8
fettx <fettx@yandex.com>
2015-07-31 11:13:37 -0300

    defining a full_title in application_helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  #Returns the full title on a per -pages basic
+  def full_title(page_title='')
+    if page_title.empty?
+      base_title
+    else
+      "#{page_title} | #{base_title}"
+    end
 end
